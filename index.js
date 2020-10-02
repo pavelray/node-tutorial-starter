a7af86194e2ed10c41cb0aa539ea97aaeabafd74
Pavel Ray <35458270+pavelray@users.noreply.github.com>
2020-10-02 20:06:45 +0530

    updated project

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
 
 const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
@@ -9,19 +10,25 @@ const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data); 
 
+
 const server = http.createServer((req,res)=> {
-    const pathName = req.url;
-    if(pathName === '/' || pathName === '/overview'){
+    const { query, pathname } = url.parse(req.url, true);
+
+    if(pathname === '/' || pathname === '/overview'){
         res.writeHead(200, {'Content-type': 'text/html'});
 
-        // const cardsHtml = dataObj.map()
+        const cardsHtml = dataObj.map(el => replaceTemplate(templateCard,el)).join('');
+        const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
 
-
-        res.end(templateOverview);
+        res.end(output);
     }
-    else if( pathName === '/product'){
-        res.end('This is Product')
-    }else if( pathName === '/api'){
+    else if( pathname === '/product'){
+        const product = dataObj[query.id];
+        const output = replaceTemplate(templateProduct,product);
+
+        res.writeHead(200, {'Content-type': 'text/html'});
+        res.end(output)
+    }else if( pathname === '/api'){
         res.writeHead(200, {
             'Content-type': 'application/json'
         });
